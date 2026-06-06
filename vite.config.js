9c6dc369e1af1b69ef6fdbaf38b9f0a721693b30
RITVIK KHADAGDE <r8877416@gmail.com>
2026-06-06 19:40:34 +0530

    Update repository name to Medx.AI

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Medx.ai/',
+  base: '/Medx.AI/',
   plugins: [react(), tailwindcss()],
 })
